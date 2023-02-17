bcd8a8031e00781cc896919f58bd6dca65572b2b
Sthembile-Tsotetsi <Sthembileshadron@gmail.com>
2023-02-17 13:50:13 -0800

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,18 @@
+/*
+ * File: 4-puts.c
+ * Auth: Sthembile Tsotetsi <Sthembileshadron@gmail.com>
+ */
+
+#include <stdio.h>
+
+/**
+ * main - Prints "Programming is like building a multilingual puzzl, followed by a new line.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+
+	return (0);
+}
