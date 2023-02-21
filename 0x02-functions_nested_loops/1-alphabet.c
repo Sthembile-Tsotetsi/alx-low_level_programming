4f9c18e98a520d0464734a37e401a19841c8aa69
Sthembile-Tsotetsi <Sthembileshadron@gmail.com>
2023-02-21 13:05:20 -0800

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,22 @@
+/*
+ * File: 1-alphabet.c
+ * Auth: Sthembile Tsotetsi
+ *
+ */
+
+#include "main.h"
+
+/**
+ * Prints the alphabet in lowercase, followed by a new line.
+ *
+ * Return: Always 0.
+ */
+void print_alphabet(void)
+{
+	char letter;
+
+	for (letter = 'a'; letter <= 'z'; letter ++)
+		_putchar(letter);
+
+	_putchar('\n');
+}
