2b60e4a2321900df4eb137fc2d1390c0ab4cf512
Sthembile-Tsotetsi <Sthembileshadron@gmail.com>
2023-06-08 18:04:31 -0700

    main.h

---
+++ 0x08-recursion/main.h
@@ -0,0 +1,19 @@
+#ifndef main.h
+#define main.h
+
+/**
+ * File: main.h
+ * Auth: Sthembile Tsotetsi
+ * Desc: Header file containing prototypes of all functions and the prototype of the function.
+ */
+
+int _putchar(char c);
+void _puts_recursion(char *s);
+void _print_rev_recursion(char *s);
+int _strlen_recursion(char *s);
+int factorial(int n);
+int _pow_recursion(int x, int y);
+int _sqrt_recursion(int n);
+int is_prime_number(int n);
+
+#endif
