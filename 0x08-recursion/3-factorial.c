fa95fd4e73446bf0659ca0fdf29741cc28b676ef
Sthembile-Tsotetsi <Sthembileshadron@gmail.com>
2023-06-08 19:09:14 -0700

    3-factorial.c

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -3,7 +3,7 @@
  * factorial - Returns the factorial of a given number.
  * @n: The number to find the factorial of.
  *
- * Retun: If n > 0 - the factorial of n.
+ * Return: If n > 0 - the factorial of n.
  *	 If n < 0 - to indicate an error.
  */
 int factorial(int n)
